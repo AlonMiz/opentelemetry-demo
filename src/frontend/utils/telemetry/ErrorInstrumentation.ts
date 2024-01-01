46c3495eac8a67d76998e7497837f96bfb68bf08
AlonMiz <alonmiz1234@gmail.com>
2024-01-01 19:47:54 +0200

    handle errors

--- src/frontend/utils/telemetry/ErrorInstrumentation.ts
+++ src/frontend/utils/telemetry/ErrorInstrumentation.ts
@@ -11,40 +11,35 @@ export class ErrorInstrumentation extends InstrumentationBase {
     const { message, stack } = error;
     return { message, stack };
   }
-  report = (error: Error, type: 'unhandledRejection' | 'error' | 'errorEvent' | 'documentError' | 'errorLog') => {
+  report(error: Error, type: 'unhandledRejection' | 'error' | 'errorEvent') {
     const span = this.tracer.startSpan('error');
-    span.setAttribute('error.type', type);
     const { message, stack } = this.parseError(error);
     span.setAttribute('error.message', message);
+    span.setAttribute('error.type', type);
     if (stack) span.setAttribute('error.stack', stack);
     addBasicAttributes(span);
     span.end();
-  };
-
-  errorListener = (event: ErrorEvent) => {
-    this.report(event.error, 'errorEvent');
-  };
-
-  unhandledRejectionListener = (event: PromiseRejectionEvent) => {
-    this.report(event.reason, 'unhandledRejection');
-  };
-
-  documentErrorListener = (event: ErrorEvent) => {
-    this.report(event.error, 'documentError');
-  };
-
-  enable() {
-    debugger;
-    window.addEventListener('unhandledrejection', this.unhandledRejectionListener);
-    window.addEventListener('error', this.errorListener);
-    document.documentElement.addEventListener('error', this.documentErrorListener, { capture: true });
   }
+  enable() {
+    window.addEventListener('error', (event: ErrorEvent) => {
+      this.report(event.error, 'error');
+    });
 
-  // disable() {
-  //   window.removeEventListener('error', this.errorListener);
-  //   window.removeEventListener('unhandledrejection', this.unhandledRejectionListener);
-  //   document.documentElement.removeEventListener('error', this.documentErrorListener, {
-  //     capture: true,
-  //   });
-  // }
+    window.addEventListener('unhandledrejection', (event: PromiseRejectionEvent) => {
+      this.report(event.reason, 'unhandledRejection');
+    });
+    document.documentElement.addEventListener(
+      'error',
+      (event: ErrorEvent) => {
+        this.report(event.error, 'error');
+      },
+      {
+        capture: true,
+      }
+    );
+
+    window.addEventListener('error', event => {
+      this.tracer.startSpan('error', { attributes: { error: event.error } }).end();
+    });
+  }
 }
